4cd190b5761a1e874d7a1b04749fa1c5a392649e
classy <classy@cs.ubc.ca>
2020-01-25 21:20:08 +0000

    Starter files

---
+++ src/test/ca/ubc/cs/cpsc210/servicecard/tests/FoodServicesCardTest.java
@@ -0,0 +1,10 @@
+package ca.ubc.cs.cpsc210.servicecard.tests;
+
+import ca.ubc.cs.cpsc210.servicecard.model.FoodServicesCard;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+// Unit tests for FoodServiceCard class
+class FoodServicesCardTest {
+
+}
